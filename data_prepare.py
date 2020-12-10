b14cf04dae54459352032dede7e2eb1e70073b6b
freefly518 <29517769@qq.com>
2020-12-10 08:29:51 +0800

    增加备注

--- data_prepare.py
+++ data_prepare.py
@@ -4,16 +4,22 @@ import random
 import numpy as np
 import tensorflow as tf
 from mfcc import *
-
+# 音频目录
 data_dir = "./wav_data/"
+# 标签目录
 data_label_dir = "./data_label/"
 
+# 训练数据集目录
 label_train_txt = "./train_label.txt"
+# 验证数据集目录
 label_val_txt = "./val_label.txt"
 
+# shuffle 训练集
 shuffle_label_train_txt = "./train_label_shuffle.txt"
 
+# 训练集tfrecord目录
 tfrecord_train = "./tfrecords/train.tfrecords"
+# 验证集 tfrecord目录
 tfrecord_val = "./tfrecords/val.tfrecords"
 
 
@@ -33,19 +39,26 @@ def _int64_feature(value):
     """Returns an int64_list from a bool / enum / int / uint."""
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-
+# 采样率16000到8000
 def resample_signal_16_to_8(origin_signal, origin_rate, rate=2):
+    """
+    param:origin_signal 原始数据,格式数组
+    param:origin_rate   原始采样率 16000
+    param:rate          转化率
+    """
     if rate > len(origin_signal):
         print("# num out of length, return arange:", end=" ")
         return origin_signal
     resampled_signal = np.array([], dtype=origin_signal.dtype)
-    seg = int(len(origin_signal) / rate)
+    seg = int(len(origin_signal) / rate) # seg = 8000
     for n in range(seg):
+        # rate * n = 2 * [1:8000] 16000的偶数位置数据,相当于数据减半，采样率是8000
         resampled_signal = np.append(resampled_signal, origin_signal[int(rate * n)])
     return int(origin_rate/rate), resampled_signal
 
-# 生成标签数据 从wav_data目录里。
-# wav_data 目录存放的是音频文件
+# 根据wav_data目录里的音频文件，生成标签数据data_label
+# wav_data      格式：文件夹：no, off, on, yes
+# data_label    格式：  文件：label_no.txt, label_off.txt, label_on.txt, label_yes.txt
 def gen_label_rm_bad():
     wav_dirs = os.listdir(data_dir)  
     for index, wav_dir in enumerate(wav_dirs):
@@ -59,8 +72,9 @@ def gen_label_rm_bad():
                 if len(signal) >= 15000 and sample_rate == 16000:
                     file_label = current_wav_file + " " + str(index) + "\n"
                     f.write(file_label)
-        f.close()
+        # f.close() 不用手动关闭
 
+# 划分数据集
 def split_train_val_label():
     label_filenames = os.listdir(data_label_dir) 
     with open(label_train_txt, "w")as fa, open(label_val_txt, "w") as fb:
@@ -68,30 +82,31 @@ def split_train_val_label():
             f1 = data_label_dir + label_file
             with open(f1, "r") as f:
                 lines = f.readlines()
-            f.close()
+            # f.close() 不用手动关闭
             for _ in range(int(len(lines) * 0.8)):
-                fa.write(lines.pop(random.randint(0, len(lines) - 1)))
+                fa.write(lines.pop(random.randint(0, len(lines) - 1)))  # lines.pop() 函数用于移除列表中的一个元素（默认最后一个元素），并且返回该元素的值
             fb.writelines(lines)
-    fa.close()
-    fb.close()
+    # fa.close() 不用手动关闭
+    # fb.close() 不用手动关闭
 
-
+# 训练集 shuffle
 def train_label_shuffle():
     lines = []
     with open(shuffle_label_train_txt, 'w') as out:
         with open(label_train_txt, 'r') as infile:
             for line in infile:
                 lines.append(line)
+        # 进行4次shuffle
         random.shuffle(lines)
         random.shuffle(lines)
         random.shuffle(lines)
         random.shuffle(lines)
         for line in lines:
             out.write(line)
-        infile.close()
-    out.close()
+    #     infile.close() 不用手动关闭
+    # out.close()
 
-
+# 生成wav、labels文件
 def get_imgs_labels(label_file):
     wav_filenames = []
     labels = []
@@ -100,17 +115,21 @@ def get_imgs_labels(label_file):
         for line in lines:
             wav_filenames.append(line.split()[0])
             labels.append(line.split()[1])
-    f.close()
+    # f.close() 不用手动关闭
     return wav_filenames, labels
 
-
+# 生成tfrecords文件
 def gennerate_terecord_file(tfrecordfilename, label_file):
     cnt = 0
+    # 获取wav文列表，label列表
     filenames, labels = get_imgs_labels(label_file)
     with tf.io.TFRecordWriter(tfrecordfilename) as writer:
         for filename, label in zip(filenames, labels):
             cnt = cnt + 1
+            # sample_rate 频率
+            # wave_data 音频数据格式数组
             sample_rate, wave_data = scipy.io.wavfile.read(filename)
+            # 采样率16000到8000
             resampled_rate, resampled_signal = resample_signal_16_to_8(wave_data, sample_rate, rate=2)
             resampled_signal = np.append(resampled_signal, np.zeros(8000 - len(resampled_signal), dtype=np.int16))
 
@@ -167,11 +186,11 @@ def gen_data_batch(file_pattern, batch_size, num_repeat=5, is_training=True):
 
 if __name__ == '__main__':
     ## Execute sequentially and everyyime only one
-    # gen_label_rm_bad()
-    # split_train_val_label()
-    # train_label_shuffle()
-    # gennerate_terecord_file(tfrecord_train, shuffle_label_train_txt)    
-    # gennerate_terecord_file(tfrecord_val, label_val_txt)      
+    # gen_label_rm_bad()        # 生成label
+    # split_train_val_label()   # 划分数据集
+    # train_label_shuffle()     # shuffle
+    # gennerate_terecord_file(tfrecord_train, shuffle_label_train_txt)  # 生成tfrecord_train
+    # gennerate_terecord_file(tfrecord_val, label_val_txt)              # 生成tfrecord_val
 
     # For test the save information of tfrecord files is OK or not
     dataset1 = gen_data_batch(tfrecord_val, 6, 2, is_training=False)
